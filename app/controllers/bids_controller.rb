ac62eba1c63059c823403eae0be90a92bdf0e1f2
Steven Cooper <stevengcooper76@gmail.com>
2016-03-17 16:50:38 -0400

    Item#show now updates without refreshing the page.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,13 +2,8 @@ class BidsController < ApplicationController
 
   def create
     @bid = Bid.new(bid_params)
-
-    if @bid.save
-      redirect_to @bid.item, notice: 'Your bid has been recorded!'
-    else
-      @item = @bid.item
-      render "items/show"
-    end
+    @bid.save
+    @item = @bid.item
   end
 
 
